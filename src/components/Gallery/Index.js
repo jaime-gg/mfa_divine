bd8f58fe140c5ae88e8146208281c03fc74d6488
jester <jaimebuddy98@gmail.com>
2022-06-17 16:04:45 -0500

    styling gallery sizes

--- src/components/Gallery/Index.js
+++ src/components/Gallery/Index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Filters from "../Filters";
 
+import './style.css';
+
 import img1 from '../../assets/1.jpg'
 import img2 from '../../assets/2.jpg'
 import img3 from '../../assets/3.jpg'
@@ -19,11 +21,11 @@ const Gallery = () => {
 
             <Filters />
 
-            <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+            <div class="row portfolio" data-aos="fade-up" data-aos-delay="100">
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-app">
                     <div class="portfolio-wrap">
-                        <img src={img1} class="img-fluid" alt="" />
+                        <img src={img1} class="g-img" alt="" />
                         <div class="portfolio-links">
                             <a href={img1} data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
@@ -33,7 +35,7 @@ const Gallery = () => {
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-web">
                     <div class="portfolio-wrap">
-                        <img src={img2} class="img-fluid" alt="" />
+                        <img src={img2} class="g-img" alt="" />
                         <div class="portfolio-links">
                             <a href={img2} data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
@@ -43,7 +45,7 @@ const Gallery = () => {
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-app">
                     <div class="portfolio-wrap">
-                        <img src={img3} class="img-fluid" alt="" />
+                        <img src={img3} class="g-img" alt="" />
                         <div class="portfolio-links">
                             <a href="{img3}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
@@ -53,7 +55,7 @@ const Gallery = () => {
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-card">
                     <div class="portfolio-wrap">
-                        <img src={img4} class="img-fluid" alt="" />
+                        <img src={img4} class="g-img" alt="" />
                         <div class="portfolio-links">
                             <a href="{img4}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
@@ -63,7 +65,7 @@ const Gallery = () => {
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-web">
                     <div class="portfolio-wrap">
-                        <img src={img5} class="img-fluid" alt="" />
+                        <img src={img5} class="g-img" alt="" />
                         <div class="portfolio-links">
                             <a href="{img5}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
@@ -73,7 +75,7 @@ const Gallery = () => {
 
                 <div class="col-lg-4 col-md-6 portfolio-item filter-app">
                     <div class="portfolio-wrap">
-                        <img src={img6} class="img-fluid" alt="" />
+                        <img src={img6} class="g-img" alt="" />
                         <div class="portfolio-links">
                             <a href="{img6}" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 3"><i class="bx bx-plus"></i></a>
                             <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
