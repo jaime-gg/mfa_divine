9d39eb04345b1d90c16ee0066e9215d96b5a7888
jester <jaimebuddy98@gmail.com>
2022-06-19 19:28:08 -0500

    developed necessary jsx elements

--- src/components/ImgData/index.js
+++ src/components/ImgData/index.js
@@ -2,9 +2,23 @@ import React, { useState } from 'react';
 import Modal from '../Modal';
 
 const ImgData = ({ category }) => {
+
+   
+
     return (
         <div>
-            
+            {isModalOpen && <Modal currentImg={currentImg} onClose={toggleModal} />}
+            <div className="flex-row">
+                {currentImgs.map((image, i) => (
+                    <img
+                        src={require(`../../assets/small/${category}/${i}.jpg`)}
+                        alt={image.name}
+                        className="img-thumbnail mx-1"
+                        key={image.name}
+                        onClick={() => toggleModal(image, i)}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
