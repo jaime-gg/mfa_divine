fb1c7346701dcceb98b2f0e8cad55cbaf6ed59ad
jester <jaimebuddy98@gmail.com>
2022-06-20 20:25:54 -0500

    filter compatability updated

--- src/components/Filters/Index.js
+++ src/components/Filters/Index.js
@@ -1,12 +1,31 @@
 import React from "react";
 
-const Filters = () => {
+const Filters = (props) => {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
 
     return (
         <div class="row" data-aos="fade-up">
             <div class="col-lg-12 d-flex justify-content-center">
-                <ul id="portfolio-flters">
-
+            <ul className="flex-row">
+                    {categories.map((category) => (
+                        <li
+                            className={`mx-1 ${currentCategory.name === category.name && `navActive`
+                                }`}
+                            key={category.name}
+                        >
+                            <span
+                                onClick={() => {
+                                    setCurrentCategory(category);
+                                }}
+                            >
+                                {(category.name)}
+                            </span>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
